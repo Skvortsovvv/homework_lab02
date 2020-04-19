904656980b7382e8b015f7dc9651f2952200b2f0
Skvortsovvv <vladimirs001@yandex.ru>
2020-04-19 19:56:48 +0300

    Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
-int main() {
+int
+main()
+{
   std::string username;
   std::cout << "Enter username: ";
   std::cin >> username;  // комментарий от пользователя
