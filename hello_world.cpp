66df6bb26e4ae54da3bf965e9a70d1ac31cdb491
Skvortsovvv <vladimirs001@yandex.ru>
2020-04-19 13:29:44 +0300

    changed code-style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-int
-main()
+int main()
 {
-  std::string username;
-  std::cout << "Enter username: ";
-  std::cin >> username; // comment from user
-  std::cout << "Hello world from " << username << std::endl;
+    std::string username;
+    std::cout << "Enter username: ";
+    std::cin >> username; // comment from user
+    std::cout << "Hello world from "<< username << std::endl;
 }
