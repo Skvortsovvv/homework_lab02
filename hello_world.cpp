5bd289477b496e9ffe61cfbdca6aa34e77e55e39
Skvortsovvv <vladimirs001@yandex.ru>
2020-04-19 19:45:18 +0300

    fixed master file

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
-int main() {
-  std::string username;
-  std::cout << "Enter username: ";
-  std::cin >> username;  // comment from user
-  std::cout << "Hello world from " << username << std::endl;
+int main()
+{
+    std::string username;
+    std::cout << "Enter username: ";
+    std::cin >> username; // comment from user
+    std::cout << "Hello world from "<< username << std::endl;
 }
